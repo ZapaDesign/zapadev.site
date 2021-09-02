3def31e8926e0a4c6f3e52d7bce1f527acb81db2
Zaparenko Sergey <zapa.poltava@gmail.com>
2021-09-03 00:45:45 +0300

    Fix Style

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,84 +27,33 @@
 
 <style lang="scss">
 
-    @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap');
 
-    :root {
-        // COLORS
-        --withe:#FFF;
-        --black: #000;
-        --gray: #626262;
-        --yellow: #ffef6c;
-        --red: #f6447d;
-        --green: #01c086;
+@import '~/assets/css/vars.scss';
+@import '~/assets/css/typography.scss';
 
-        --body-color: #fff;
-        --aside-color: #192b39;
-        --hamburger-color: #aaa;
+* {
+    margin: 0;
+    padding: 0;
+}
 
-        --main-color: var(--green);
-        --accent-color: var(--red);
-
-        --main-text-color: #262626;
-        --sub-text-color: #626262;
-        --aside-text-color: #fff;
-
-        // TYPOGRAPHY
-
-        --VinMonoPro: 'Vin Mono Pro';
-        --OpinionProCondensed: 'OpinionProCondensed';
-        --OpinionProExtraCondensed: 'OpinionProExtraCondensed';
-
-        --main-font: 500 1rem/1.5 var(--VinMonoPro);
-        --acc-font: 600 0.75rem/1.5 var(--VinMonoPro);
-        --add-font: 500 1rem/1.5 var(--OpinionProCondensed);
-        --pheader-font: 400 2rem/1.5 var(--OpinionProExtraCondensed);
+#app {
+    display: flex;
+    background-color: var(--body-color);
+    min-height: 100vh;
+    font: var(--main-font);
+    font-size: 16px;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: var(--main-text-color);
+    position: relative;
+    overflow: hidden;
+    //height: 100vh;
+    @media (min-width: 1920px){
+    font-size: vw(16px);
     }
-
-
-    .dark-mode {
-        // COLORS
-        --body-color: #1d1d25;
-        --aside-color: #222229;
-
-        --main-color: #06d293;
-        --accent-color: #f6447d;
-
-        --main-text-color: #fff;
-        --sub-text-color: #9d9d9d;
-        --aside-text-color: #fff;
-        --head-text-color: var(--yellow);
-
-        // TYPOGRAPHY
-
-        --main-font: 300 1rem/1.5 var(--VinMonoPro);
-        --acc-font: 400 0.75rem/1.5 var(--VinMonoPro);
-        --add-font: 300 1rem/1.5 var(--OpinionProCondensed);
-        --pheader-font: 100 2rem/1.5 var(--OpinionProExtraCondensed);
-    }
-
-    * {
-        margin: 0;
-        padding: 0;
-    }
-
-    #app {
-        display: flex;
-        background-color: var(--body-color);
-        min-height: 100vh;
-        font: var(--main-font);
-        font-size: 16px;
-        line-height: 1.5;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: var(--main-text-color);
-        position: relative;
-        overflow: hidden;
-        //height: 100vh;
-        @media (min-width: 1920px){
-        font-size: vw(16px);
-        }
-    }
+}
 
 .main {
 	scroll-behavior: smooth;
@@ -216,38 +165,6 @@
 
 }
 
-
-
-
-
-ul {
-	list-style: none;
-}
-
-a,
-a:active,
-a:visited {
-    color: var(--main-text-color);
-	text-decoration: none;
-}
-
-img {
-	max-width: 100%;
-}
-
-pre,
-code {
-	white-space: break-spaces;
-}
-
-strong {
-    color: var(--accent-color);
-}
-
-em {
-    color: var(--main-color);
-}
-
 ::-webkit-scrollbar-button {
 	background-repeat: no-repeat;
 	width: 6px;
@@ -273,151 +190,6 @@ em {
 
 ::-webkit-scrollbar {
 	width: 4px;
-}
-
-.nuxt-content {
-
-
-    h1, h2, h3, h4, h5, h6, p, ul, ol, blockquote, hr, table, pre,
-    .two-columns, .three-columns {
-        max-width: 920px;
-        margin: 0 auto;
-    }
-    .full {
-        h1, h2, h3, h4, h5, h6, p, ul, ol, blockquote, hr, table, pre,
-        .two-columns, .three-columns {
-            max-width: unset;
-        }
-    }
-
-    h1, h2, h3, h4, h5, h6 {
-        color: var(--head-text-color);
-        border-bottom: 1px solid var(--sub-text-color);
-    }
-
-    h3, h4, h5, h6 {
-        //display: table;
-        border-bottom: none;
-        text-decoration: underline;
-        text-underline-offset: 8px;
-        text-decoration-thickness: 1px;
-        text-decoration-color: var(--sub-text-color);  //
-    }
-
-	h1,
-	h2,
-	h3,
-	h4 {
-		position: relative;
-		font-weight: 700;
-		margin-bottom: 2rem;
-        //color: var(--main-text-color);
-		padding-top: 2rem;
-	}
-
-	h1 {
-        color: var(--accent-color);
-		font-size: 32px;
-        @media (min-width: 1920px){
-            font-size: vw(32);
-        }
-	}
-
-	h2 {
-		font-size: 28px;
-        @media (min-width: 1920px){
-            font-size: vw(28);
-        }
-	}
-
-	h3 {
-		font-size: 24px;
-        @media (min-width: 1920px){
-            font-size: vw(24);
-        }
-	}
-
-	h4 {
-		font-size: 22px;
-        @media (min-width: 1920px){
-            font-size: vw(22);
-        }
-	}
-
-    h5 {
-        font-size: 20px;
-        @media (min-width: 1920px){
-            font-size: vw(20);
-        }
-    }
-
-    h6 {
-        font-size: 18px;
-        @media (min-width: 1920px){
-            font-size: vw(18);
-        }
-    }
-
-	ul {
-		list-style: disc;
-	}
-
-	ul,
-	ol {
-		//margin: 1rem 0;
-		//padding-left: 40px;
-	}
-
-	ul,
-	ol > li:last-child {
-		margin-bottom: 10px;
-	}
-
-	table {
-		margin-bottom: 10px;
-		border-collapse: collapse;
-	}
-
-	th,
-	td {
-		padding: 0.5rem;
-	}
-
-	p {
-		margin-bottom: 1rem;
-
-		img {
-			display: block;
-            border-radius: 10px;
-            @media (min-width: 1920px){
-                border-radius: vw(10);
-            }
-		}
-
-		img + em {
-			display: block;
-			margin-top: 0.5rem;
-			font-size: 12px;
-		}
-	}
-
-    .two-columns {
-        column-count: 1;
-        column-gap: 40px;
-        //column-rule: 1px solid var(--sub-text-color);
-        @media (min-width: 720px){
-            column-count: 2;
-        }
-    }
-
-    .three-columns {
-        column-count: 1;
-        column-gap: 30px;
-        //column-rule: 1px solid var(--sub-text-color);
-        @media (min-width: 720px){
-            column-count: 3;
-        }
-    }
 }
 
 .ScrollToUpBtn {
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -94,15 +94,6 @@ export default {
         margin-left: auto;
         margin-right: auto;
 
-        p {
-            font-size: 1rem;
-            line-height: 1.5;
-        }
-
-        pre {
-            font-size: 1rem;
-        }
-
         .title {
             font-size: inherit;
         }
@@ -110,69 +101,6 @@ export default {
         &.slideLeftInOut {
             animation-duration: 0.5s;
             animation-delay: 0.3s;
-        }
-
-        p code,
-        li code {
-            // color: #1a202c;
-            // font-weight: 400;
-            // font-size: .875em;
-            // color: var(--white);
-            background-color: #282a36;
-            padding: 0.25rem;
-            border-width: 1px;
-            border-color: var(--sub-text-color);
-            border-radius: 0.25rem;
-        }
-
-        blockquote {
-            border: 1px solid var(--sub-text-color);
-            padding: 1rem;
-            padding-bottom: 0;
-            margin-top: 1rem;
-            margin-bottom: 1rem;
-            border-radius: 0.25rem;
-        }
-
-        hr {
-            margin-top: 2rem;
-            margin-bottom: 1rem;
-            background-color: $linesColor;
-            height: 1px;
-            border: none;
-        }
-
-        table {
-            text-align: left;
-            border-collapse: collapse;
-            margin: 10px;
-            border: 1px solid $linesColor;
-        }
-
-        table th {
-            color: var(--main-color);
-            font-weight: 300;
-            border-bottom: 1px solid $linesColor;
-            border-right: 1px solid $linesColor;
-            padding: 12px 17px;
-        }
-
-        table td {
-            border-bottom: 1px solid $linesColor;
-            border-right: 1px solid $linesColor;
-            padding: 7px 17px;
-        }
-
-        table tr:last-child td {
-            border-bottom: none;
-        }
-
-        table td:last-child {
-            border-right: none;
-        }
-
-        table tr:hover td {
-            text-decoration: underline;
         }
     }
 }
