056381407e4d71beb3a96eeab5a9591d1cdc9a51
Serhii Zaparenko <46490612+ZapaDesign@users.noreply.github.com>
2021-09-03 11:35:07 +0300

    Update VariableFontsAnicons.vue

--- components/global/VariableFontsAnicons.vue
+++ components/global/VariableFontsAnicons.vue
@@ -1,5 +1,8 @@
 <template>
-
+    <div>
+        <div class="icon">5</div>
+        <div class="icon">H</div>
+    </div>
 </template>
 
 <script>
@@ -7,5 +10,12 @@
 </script>
 
 <style lang="scss">
-
+    @import url('https://res.cloudinary.com/dr6lvwubh/raw/upload/v1581441981/Anicons/anicons-regular.css');
+    
+    .icon {
+        font-family: "Anicons Regular", sans-serif;
+        font-variation-settings: "TIME" 1;
+        transition: font-variation-settings 0.4s ease;
+        cursor: pointer;
+    }
 </style>
