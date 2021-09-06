1d01b72c75bca6b7cf0bcb94edb9bdc1c4111f98
Serhii Zaparenko <46490612+ZapaDesign@users.noreply.github.com>
2021-09-06 14:51:52 +0300

    Update _slug.vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -6,7 +6,7 @@
 			<article class="article__content slideLeftInOut">
                 <ul>
                     <li
-                        v-for="link of article.toc"
+                        v-for="link of post.toc"
                         :key="link.id"
                         :class="{ 'toc2': link.depth === 2, 'toc3': link.depth === 3 }"
                     >
